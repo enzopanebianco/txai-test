badbd39e2364c7e5995fc40100a11457e1a7f031
Enzo Romano Panebianco Enzo <enzinho.983@gmail.com>
2022-04-17 19:28:31 -0300

    mostrando erro de login

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,31 +1,43 @@
-import React, { Fragment, useState } from "react";
-import { Button, Container, InputGroup, FormControl, FloatingLabel, FormGroup } from "react-bootstrap";
-import { useNavigate } from "react-router";
-import {api} from "../../services/api";
+import React, { Fragment, useState } from "react"
+import { Button, Container, InputGroup, FormControl, FloatingLabel, FormGroup } from "react-bootstrap"
+import { useNavigate } from "react-router"
+import { api } from "../../services/api"
 export default function Login() {
-    
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
+
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorLogin,setErrorLogin] = useState(false)
+
     const navigate = useNavigate();
 
-    const handleLogin = async() =>{
-        const {data} = await api.post("/login",{
+    const handleLogin = async () => {
+        try{
+        const { data } = await api.post("/login", {
             name,
             password
         })
 
-       const {token} = data;
+        const { token } = data
+        if (token) {
+            localStorage.setItem('user', token)
 
-       localStorage.setItem('user',token);
-
-       navigate('/products')
+            navigate('/products')
+        }
+    }
+    catch(error){
+        setErrorLogin(true)
+    }
+       
     }
 
     return (
         <Fragment>
-      
+
             <Container className="w-50 p-3 mt-5" >
                 <h1>Login</h1>
+                {
+                    errorLogin&& <strong class="text-danger">Usuário ou senha incorretos</strong>
+                }
                 <FormGroup>
                     <InputGroup>
                         <FloatingLabel label="Usuário" className="mb-3" controlId="floatingInput" />
@@ -33,7 +45,7 @@ export default function Login() {
                             placeholder="Usuário"
                             className="mb-3 mt-3 col-6"
                             type="text"
-                            onChange={(e)=>setName(e.target.value)}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </InputGroup>
                     <InputGroup>
@@ -42,13 +54,13 @@ export default function Login() {
                             placeholder="Senha"
                             className="mb-3"
                             type="password"
-                            onChange={(e)=>setPassword(e.target.value)}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </InputGroup>
-                    <Button onClick={()=>handleLogin()}>Entrar</Button>
+                    <Button onClick={() => handleLogin()}>Entrar</Button>
                 </FormGroup>
             </Container>
-           
+
         </Fragment>
     )
 }
